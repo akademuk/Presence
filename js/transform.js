f6873527d19f6deb959c564d0310ce0ef9060052
Oleg Fedorov <akademuk24@gmail.com>
2022-11-01 15:01:49 +0200

    ss

--- js/transform.js
+++ js/transform.js
@@ -1,33 +1,38 @@
 $(document).ready(function () {
-if ($(window).width() > 768) {
-    $(window).resize(function () {
-        calculateNewScale();
-    });
-    calculateNewScale();
-    function calculateNewScale() {
-        var percentageOn1 = $(window).width() / 1240;
-        $(".wrapper").css({
-            "-moz-transform": "scale(" + percentageOn1 + ")",
-            "-webkit-transform": "scale(" + percentageOn1 + ")",
-            transform: "scale(" + percentageOn1 + ")",
-            "transform-origin": "left top",
-        });
-    }
-}
+    if ($(window).width() > 768) {
 
-if ($(window).width() < 768) {
-$(window).resize(function () {
-    calculateNewScale();
-});
-calculateNewScale();
-function calculateNewScale() {
-    var percentageOn1 = $(window).width() / 768;
-    $(".wrapper").css({
-        "-moz-transform": "scale(" + percentageOn1 + ")",
-        "-webkit-transform": "scale(" + percentageOn1 + ")",
-        transform: "scale(" + percentageOn1 + ")",
-        "transform-origin": "left top",
-    });
-}
-}
+        $(window).resize(function () {
+            calculateNewScale();
+        });
+
+        calculateNewScale();
+        function calculateNewScale() {
+            var percentageOn1 = $(window).width() / 1240;
+            $(".wrapper").css({
+                "-moz-transform": "scale(" + percentageOn1 + ")",
+                "-webkit-transform": "scale(" + percentageOn1 + ")",
+                transform: "scale(" + percentageOn1 + ")",
+                "transform-origin": "left top",
+            });
+        }
+    }
+
+    if ($(window).width() < 768) {
+
+        $(window).resize(function () {
+            calculateNewScale();
+        });
+
+        calculateNewScale();
+
+        function calculateNewScale() {
+            var percentageOn1 = $(window).width() / 768;
+            $(".wrapper").css({
+                "-moz-transform": "scale(" + percentageOn1 + ")",
+                "-webkit-transform": "scale(" + percentageOn1 + ")",
+                transform: "scale(" + percentageOn1 + ")",
+                "transform-origin": "left top",
+            });
+        }
+    }
 });
